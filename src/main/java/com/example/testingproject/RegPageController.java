4801f66d30b1505b49cad1b7eaab004f289ddacf
seif medhat <seifmedhat2001@gmail.com>
2023-06-09 04:00:46 +0300

    reg page updated

--- src/main/java/com/example/testingproject/RegPageController.java
+++ src/main/java/com/example/testingproject/RegPageController.java
@@ -1,23 +1,23 @@
 package com.example.testingproject;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
-public class RegPageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegPageController implements Initializable {
 
     @FXML
     private Button Donebtn;
-    @FXML
-    private RadioButton femalebtn;
     @FXML
     private Scene scene;
     @FXML
@@ -25,60 +25,54 @@ public class RegPageController {
     @FXML
     private Parent root;
     @FXML
-    private RadioButton malebtn;
-    @FXML
     private TextField Mobiletxt;
-
     @FXML
     private TextField Nametxt;
-
     @FXML
     private TextField NationalIDtxt;
-
     @FXML
     private PasswordField Passwordtxt;
-
     @FXML
     private TextField Usernametxt;
+    @FXML
+    private Label errortxt;
+    @FXML
+    private ComboBox<String > genderbox;
 
-//    private String username = Usernametxt.getText();
-//    private String password = Passwordtxt.getText();
-//    private String nationalid = NationalIDtxt.getText();
-//    private String mobile = Mobiletxt.getText();
-////    private String male = malebtn.getText();
-////    private String female = femalebtn.getText();
-//    private String name = Nametxt.getText();
-
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        genderbox.setItems(FXCollections.observableArrayList("Male","Female"));
+    }
 
     @FXML
-    void Regbtnclicked(ActionEvent event){
+    void Regbtnclicked(ActionEvent event) {
         String username = Usernametxt.getText();
         String password = Passwordtxt.getText();
         String nationalid = NationalIDtxt.getText();
         String mobile = Mobiletxt.getText();
-//      String male = malebtn.getText();
-//      String female = femalebtn.getText();
+        String gender = genderbox.getValue();
         String name = Nametxt.getText();
-        if(malebtn.isSelected()) {
-            Online_Bank.createAccount(name, nationalid, username, password, mobile, malebtn.getText());
+        if (username == null || password == null || nationalid == null || mobile == null || gender == null || name == null) {
+           errortxt.setText("please enter all the data required");
         }
         else {
-            Online_Bank.createAccount(name, nationalid, username, password, mobile, femalebtn.getText());
-        }
-        try {
-            FXMLLoader loader;
-            loader = new FXMLLoader(getClass().getResource("Login.fxml"));
-            root = loader.load();
+            Online_Bank.createAccount(name, nationalid, username, password, mobile, gender);
 
-            LoginController loginController = loader.getController();
+            try {
+                FXMLLoader loader;
+                loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+                root = loader.load();
 
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        }
-        catch (Exception e){
-            e.printStackTrace();
+                LoginController loginController = loader.getController();
+
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
+
 }
